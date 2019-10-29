7897bae24a25590bc3410e296cd5e53ecab4bacb
Шадикьян Руслан Артемович <@corp.itstep>
2019-10-29 17:54:36 +0600

    initial

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Columns()
+        {
+            return View();
+        }
+
+        public ActionResult LeftSideBar()
+        {
+            return View();
+        }
+
+        public ActionResult RightSideBar()
+        {
+            return View();
+        }
+
+        public ActionResult WithoutSideBar()
+        {
+            return View();
+        }
+    }
+}
